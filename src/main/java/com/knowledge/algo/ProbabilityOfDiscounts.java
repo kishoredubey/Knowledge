26f8b0f739c32ab68382168a5441889d410549df
kishore dubey <kishore.dubey84@gmail.com>
2022-03-13 16:01:02 +0800

    more exercises

--- src/main/java/com/knowledge/algo/ProbabilityOfDiscounts.java
+++ src/main/java/com/knowledge/algo/ProbabilityOfDiscounts.java
@@ -4,45 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.SplittableRandom;
 
-class DiscountObject{
-    public int discountPercentage;
-    public int probability;
+class ProbabilityRange {
+    int begin;
+    int end;
 
-    public DiscountObject(int discountPercentage, int probability) {
+    public ProbabilityRange(int begin, int end) {
+        this.begin = begin;
+        this.end = end;
+    }
+}
+
+class DiscountObject {
+    int discountPercentage;
+    ProbabilityRange probability;
+
+    public DiscountObject(int discountPercentage, ProbabilityRange probability) {
         this.discountPercentage = discountPercentage;
         this.probability = probability;
     }
-
-    @Override
-    public String toString() {
-        return "DiscountObject{" +
-                "discountPercentage=" + discountPercentage +
-                ", probability=" + probability +
-                '}';
-    }
 }
+
 public class ProbabilityOfDiscounts {
     public static void main(String[] args) {
         List<DiscountObject> objects = new ArrayList<>();
 
-        objects.add(new DiscountObject(30,10));
-        objects.add(new DiscountObject(20,20));
-        objects.add(new DiscountObject(10,70));
-        for (int i=0; i<20; i++) {
+
+        objects.add(new DiscountObject(30, new ProbabilityRange(0, 1)));
+        objects.add(new DiscountObject(20, new ProbabilityRange(2, 20)));
+        objects.add(new DiscountObject(10, new ProbabilityRange(20, 100)));
+        for (int i = 0; i < 20; i++) {
             System.out.println(findDiscount(objects));
         }
     }
 
-    private static DiscountObject findDiscount(List<DiscountObject> objects) {
+    private static int findDiscount(List<DiscountObject> objects) {
         SplittableRandom generator = new SplittableRandom();
-        int probability = generator.nextInt(0,100);
+        int probability = generator.nextInt(0, 100);
         DiscountObject out = null;
-        for (DiscountObject object: objects){
-            if (object.probability > probability) {
+        for (DiscountObject object : objects) {
+            if (object.probability.begin <= probability && object.probability.end >= probability) {
                 out = object;
                 break;
             }
         }
-        return out;
+        return out.discountPercentage;
     }
 }
